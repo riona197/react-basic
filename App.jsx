77bdca5ee49f684a40e4efcecbf7a88cc0f6270c
riona <riona.nakano0118@gmail.com>
2025-05-03 17:30:14 +0900

    section5 練習問題

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import reactLogo from './assets/react.svg';
 import styles from './App.module.css';
 import { Button } from './components/Button';
+import { Profile } from './components/Profile';
 
 export const App = () => {
 	return (
@@ -12,6 +13,8 @@ export const App = () => {
 			<Button />
 			<Button />
 			<Button />
+			<hr />
+			<Profile />
 		</div>
 	);
 };
