5d62054fc194d67aca5fb36410681d7219acc0f7
riona <riona.nakano0118@gmail.com>
2025-05-03 17:28:48 +0900

    section4 練習問題

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import reactLogo from './assets/react.svg';
 import styles from './App.module.css';
+import { Button } from './components/Button';
 
 export const App = () => {
 	return (
@@ -7,6 +8,10 @@ export const App = () => {
 			<img src={reactLogo} className={styles['react-logo']} />
 			<h1>Hello React!</h1>
 			{/* 以下に練習問題の回答を記述してください */}
+			<hr />;
+			<Button />
+			<Button />
+			<Button />
 		</div>
 	);
 };
