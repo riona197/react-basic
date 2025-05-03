7fab5feea6c4fa6d45062798a179e6d67b6ad90c
riona <riona.nakano0118@gmail.com>
2025-05-03 22:17:18 +0900

    section6 練習問題

--- App.jsx
+++ App.jsx
@@ -2,6 +2,8 @@ import reactLogo from './assets/react.svg';
 import styles from './App.module.css';
 import { Button } from './components/Button';
 import { Profile } from './components/Profile';
+import { Country } from './components/Country';
+import { Card } from './components/Card';
 
 export const App = () => {
 	return (
@@ -15,6 +17,15 @@ export const App = () => {
 			<Button />
 			<hr />
 			<Profile />
+			<hr />
+			<Card>
+				<Country
+					info={{
+						imgUrl: 'https://www.civillink.net/sozai/kokki100/pics2654.gif',
+						name: 'アメリカ',
+					}}
+				/>
+			</Card>
 		</div>
 	);
 };
