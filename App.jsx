0e123e5458bf02b3c65de8c005deca4ca440994c
riona <riona.nakano0118@gmail.com>
2025-05-04 19:49:40 +0900

    section9 練習問題

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ import { Country } from './components/Country';
 import { Card } from './components/Card';
 import { TodoItem } from './components/TodoItem';
 import { LanguageList } from './components/LanguageList';
+import { AlertButton } from './components/AlertButton';
 
 export const App = () => {
 	return (
@@ -36,6 +37,8 @@ export const App = () => {
 			</ul>
 			<hr />
 			<LanguageList />
+			<hr />
+			<AlertButton />
 		</div>
 	);
 };
