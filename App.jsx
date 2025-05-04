daa0d65e77e54ca25918329bd2dd0f6be36e1ccc
riona <riona.nakano0118@gmail.com>
2025-05-04 10:50:41 +0900

    section7 練習問題

--- App.jsx
+++ App.jsx
@@ -4,6 +4,7 @@ import { Button } from './components/Button';
 import { Profile } from './components/Profile';
 import { Country } from './components/Country';
 import { Card } from './components/Card';
+import { TodoItem } from './components/TodoItem';
 
 export const App = () => {
 	return (
@@ -26,6 +27,12 @@ export const App = () => {
 					}}
 				/>
 			</Card>
+			<hr />
+			<ul>
+				<TodoItem name="Reactの学習" isToday={true} isCompleted={true} />
+				<TodoItem name="HTMLの復習" isToday={true} isCompleted={false} />
+				<TodoItem name="React Hooksの予習" isToday={false} isCompleted={false} />
+			</ul>
 		</div>
 	);
 };
