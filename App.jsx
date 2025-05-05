c173936133bbee2dad56cbde6300d1586f4dee44
riona <riona.nakano0118@gmail.com>
2025-05-05 10:13:38 +0900

    section11 練習問題

--- App.jsx
+++ App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from './components/TodoItem';
 import { LanguageList } from './components/LanguageList';
 import { AlertButton } from './components/AlertButton';
 import { InputField } from './components/InputField';
+import { CharacterImage } from './components/CharacterImage';
 
 export const App = () => {
 	return (
@@ -42,6 +43,8 @@ export const App = () => {
 			<AlertButton />
 			<hr />
 			<InputField />
+			<hr />
+			<CharacterImage />
 		</div>
 	);
 };
